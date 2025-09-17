298fe9a53c8607b300bbae76561721a560b7f58c
BetsyZhang0 <160428307+randorin@users.noreply.github.com>
2025-09-17 13:40:20 -0400

    convert to slash commands

--- index.py
+++ index.py
@@ -2,6 +2,8 @@ import discord
 import logging
 import os
 from dotenv import load_dotenv
+from discord.ext import commands
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,43 +15,41 @@ intents.message_content = True
 #setup logging
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 # create client
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='o!', intents=intents)
 # define welcome channel
 welcome_channel_id = 1417664212645314655
 # create events
 
 # login to client
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    await bot.tree.sync()
+    print(f'We have logged in as {bot.user}')
+    print('Slash commands have been synced!')
 
 # auto send message to specific channel when user joins server
-@client.event
+@bot.event
 async def on_member_join(member):
     print("A new member joined the server.")
-    channel = client.get_channel(welcome_channel_id)
+    channel = bot.get_channel(welcome_channel_id)
     if channel:
         await channel.send(f"Welcome {member.mention} to **{member.guild.name}**! 👋")
     else:
        print('Specified channel does not exist.')
 
 # commands
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('o!greet'):
-        await message.channel.send("Welcome to GenTalks! Please read the rules thoroughly and have a fun time~ <#1361929072158179479>")
-
+@bot.tree.command(name="welcome", description="Sends a welcome message.")
+async def welcome_message(interaction: discord.Interaction):
+    await interaction.response.send_message("Welcome to GenTalks! Please read the rules thoroughly and have a fun time~ <#1361929072158179479>")
+    
+@bot.tree.command(name="member_count", description="View number of total members in this server.")
+async def member_count(interaction: discord.Interaction):
     # total server member count
-    if message.content.startswith('o!membercount'):
-        guild = message.guild
-        member_count = guild.member_count
-        await message.channel.send(f"The server currently has **{member_count}** members.")
- 
-        
+    guild = interaction.guild
+    member_count = guild.member_count
+    await interaction.response.send_message(f"**{guild.name}** currently has **{member_count}** members.")
 
 
 
 discord.utils.setup_logging(level=logging.INFO, root=False)
-client.run(TOKEN)
+bot.run(TOKEN)
